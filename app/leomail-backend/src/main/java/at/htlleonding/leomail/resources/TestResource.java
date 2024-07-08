e8a368fc949a70f061f5cdb683dec29011dd51b1
Tommy Neumaier <neumaier.tommy@gmail.com>
2024-07-08 17:18:46 +0200

    testing: added test resource for vue studying

---
+++ app/leomail-backend/src/main/java/at/htlleonding/leomail/resources/TestResource.java
@@ -0,0 +1,31 @@
+package at.htlleonding.leomail.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Path("test")
+public class TestResource {
+    private List<String> list = new LinkedList<>();
+
+    public TestResource() {
+        list.add("Hello");
+        list.add("World");
+    }
+
+    @GET
+    @Path("get")
+    public List<String> get() {
+        return list;
+    }
+
+    @POST
+    @Path("add")
+    public String add(String s) {
+        list.add(s);
+        return s;
+    }
+}
