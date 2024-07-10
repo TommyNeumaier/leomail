8110838a7718ec407d821fcc33ef79f98b344408
Tommy Neumaier <neumaier.tommy@gmail.com>
2024-07-10 11:45:02 +0200

    dev: quick fix, missing file contents

--- app/leomail-backend/src/main/java/at/htlleonding/leomail/entities/UsedTemplate.java
+++ app/leomail-backend/src/main/java/at/htlleonding/leomail/entities/UsedTemplate.java
@@ -2,19 +2,22 @@ package at.htlleonding.leomail.entities;
 
 import com.arjuna.ats.jta.exceptions.NotImplementedException;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 
 import java.time.LocalDateTime;
 
 @Entity
 public class UsedTemplate extends Template {
     public LocalDateTime sentOn;
+
+    @ManyToOne
     public Account sentBy;
 
     public UsedTemplate() throws NotImplementedException {
         throw new NotImplementedException();
     }
 
-    public UsedTemplate(String name, String headline, String content, Account createdBy, LocalDateTime sentOn, Account sentBy) {
+    public UsedTemplate(String name, String headline, String content, String createdBy, LocalDateTime sentOn, Account sentBy) {
         super(name, headline, content, createdBy);
         this.sentOn = sentOn;
         this.sentBy = sentBy;
