1cb89490d409861a2c94bdaf6653868c9b0c1fdc
Tommy Neumaier <neumaier.tommy@gmail.com>
2024-07-10 11:36:35 +0200

    dev: quick fix

--- app/leomail-backend/src/main/resources/import.sql
+++ app/leomail-backend/src/main/resources/import.sql
@@ -1,9 +1,9 @@
 INSERT INTO account(username, departement, position) VALUES ('IT200285', 'ITM', 'Student');
 INSERT INTO account(username, departement, position) VALUES ('IT200274', 'ITM', 'Student');
 
-INSERT INTO template(id, content, headline, name, createdby_username) VALUES
-(1, 'Das ist der Content', 'Headline #1', 'Die beste Vorlage', 'IT200285'),
-(2, 'Das ist der zweite Content', 'Headline #2', 'Die zweitbeste Vorlage', 'IT200274');
+INSERT INTO template(id, dtype, content, headline, name, createdby) VALUES
+(1, '','Das ist der Content', 'Headline #1', 'Die beste Vorlage', 'IT200285'),
+(2, '','Das ist der zweite Content', 'Headline #2', 'Die zweitbeste Vorlage', 'IT200274');
 
 INSERT INTO templategreeting(id, content) VALUES (1, 'Hallo, ich bin ein Gruß!'),
 (2, 'Hallo, ich bin ein weiterer Gruß!'),
