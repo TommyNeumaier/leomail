471e566a06f43cfb3ecdfb0dc3c8aabbe09c2063
Lana Sekerija <sekerija.lana@gmail.com>
2024-07-11 11:23:58 +0200

    fix

--- app/leomail-frontend/src/main.ts
+++ app/leomail-frontend/src/main.ts
@@ -5,10 +5,11 @@ import '@vueup/vue-quill/dist/vue-quill.snow.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import axios from 'axios';
-const app = createApp(App)
-app.component('QuillEditor', QuillEditor)
+import App from "@/App.vue";
+import router from "@/router/router";
 
 const app = createApp(App);
+app.component('QuillEditor', QuillEditor)
 
 const pinia = createPinia();
 app.use(pinia);
